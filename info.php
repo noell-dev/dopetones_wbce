4d88752c4561c7837085c85b77758545a52e68b9
noelli <tobiah@noell.li>
2022-02-18 11:04:39 +0100

    inital commit

---
+++ info.php
@@ -0,0 +1,49 @@
+<?php
+
+$template_directory = 'dopetrope';
+$template_name = 'Dopetrope Edit by HTML5 UP and noelli';
+$template_version = '1.1';
+$template_platform = '1.4';
+$template_author = 'html5up.net | @ajlkn, WBCE portation by Florian Meerwinck coloredit by Tobiah Nöll';
+$template_license = 'Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)';
+$template_description = 'Template with optional banner / intro, left/right sidebar, 2 + 3 columns in footer. For optimized results you need some more HTML around your contents, see snippets.txt';
+
+$block[1] = 'Main';
+$block[2] = 'Boxed Right Sidebar';
+$block[20] = 'Right Sidebar w/o box';
+$block[3] = 'Left Sidebar';
+$block[30] = 'Left Sidebar w/o box';
+$block[4] = 'Banner';
+$block[5] = 'Intro';
+$block[6] = 'Full width content bottom';
+$block[7] = 'Footer left (1st row)';
+$block[8] = 'Footer right (1st row)';
+$block[9] = 'Footer 1st third (2nd row)';
+$block[10] = 'Footer 2nd third (2nd row)';
+$block[11] = 'Footer 3rd third (2nd row)';
+$block[99] = 'no output';
+
+$menu[1] = 'Navigation';
+$menu[2] = 'Footer';
+$menu[99] = 'not in Nav.';
+
+if (LANGUAGE == "DE") {
+	$block[1] = 'Hauptblock';
+	$block[2] = 'Seitenspalte rechts mit Box';
+	$block[20] = 'Seitenspalte rechts ohne Box';
+	$block[3] = 'Seitenspalte links mit Box';
+	$block[30] = 'Seitenspalte links ohne Box';
+	$block[4] = 'Banner';
+	$block[5] = 'Intro';
+	$block[6] = 'Breiter Content unten';
+	$block[7] = 'Footer links (1. Reihe)';
+	$block[8] = 'Footer rechts (2. Reihe)';
+	$block[9] = 'Footer 1/3 (2. Reihe)';
+	$block[10] = 'Footer 2/3 (2. Reihe)';
+	$block[11] = 'Footer 3/3 (2. Reihe)';
+	$block[99] = 'nicht zeigen';
+
+	$menu[1] = 'Navigation';
+	$menu[99] = 'not in Nav.';
+}
+
